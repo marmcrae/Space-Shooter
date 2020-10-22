f6ff4587f1140d65a7c1225a4b8c96276d0f4f10
marmcrae <mcrae.marquie@gmail.com>
2020-10-22 08:23:29 -1000

    player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       //take the current position and set it to (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+
+    void CalculateMovement()
+    {
+
+
+        //*******create user input*******
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // moves player one unit (meter) to the right, left, up, and down based off user input
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+
+        //*******create player bounds & wrap*******
+
+        //player bounds
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        //player wrap
+        if (transform.position.x > 11f)
+        {
+            transform.position = new Vector3(-11f, transform.position.y, 0);
+
+        }
+        else if (transform.position.x < -11f)
+        {
+            transform.position = new Vector3(11f, transform.position.y, 0);
+
+        }
+    }
+}
